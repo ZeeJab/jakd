d61e00d815d89d53e01e1d90e60bfc004fa1b0d3
Zahra Jabini <zahra.arch@gmail.com>
2012-12-01 11:00:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jakd::Application.config.secret_token = '6c7097d5661033d416e1689171224d02e6976c2bf3358c21c1c026c946a4b0a9225ffb41d97b51c88fc983abea6180478f09da1d5d7d1fbfe0c090fa6256f3dc'
